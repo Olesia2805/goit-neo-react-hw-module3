beba4ce45feaa6f37aba8a3b9ff53081e21870a2
Olesia2805 <vysotina2002@gmail.com>
2024-10-14 19:38:53 +0200

    added saving to localStorage using useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,20 @@ import SearchBox from './components/SearchBox/SearchBox';
 import ContactList from './components/ContactList/ContactList';
 import Notification from './components/Notification/Notification';
 import contactDB from './contactsDB.json';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import appCss from './App.module.css';
 import { nanoid } from 'nanoid';
 
 const App = () => {
-  const [contacts, setContacts] = useState(contactDB);
+  const [contacts, setContacts] = useState(() => {
+    const items = JSON.parse(localStorage.getItem('items'));
+    return items || contactDB;
+  });
   const [search, setSearch] = useState('');
+
+  useEffect(() => {
+    localStorage.setItem('items', JSON.stringify(contacts));
+  }, [contacts]);
 
   const deleteContact = contactID => {
     setContacts(prevContact => {
